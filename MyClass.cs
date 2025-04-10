b9d1151e653ca71799b448202eff3663b0d280cd
bsmith243 <131480912+bsmith243@users.noreply.github.com>
2025-04-09 20:47:28 -0500

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Extra
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            li = []
+li.append(1)
+li.append(100)
+li.append(23493)
+print(li)
+        }
+    }
+}
